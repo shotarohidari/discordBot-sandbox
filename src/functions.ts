ab58fa8accf5547034055312d108cf0f090b7dfd
左翔太朗 <zuoxiangtailang@sashoutarous-MacBook-Air.local>
2022-08-25 18:51:18 +0900

    コミットミスった

--- src/functions.ts
+++ src/functions.ts
@@ -1,13 +1,14 @@
-import { TextChannel } from "discord.js"
-import { XMLParser } from "fast-xml-parser"
+import { TextChannel } from "discord.js";
+import { XMLParser } from "fast-xml-parser";
 
 export const sendNHKNews = async (channel: TextChannel) => {
-  const res = await fetch("https://www.nhk.or.jp/rss/news/cat0.xml")
-  const text = await res.text()
-  const parser = new XMLParser()
-  const data = parser.parse(text)
-  const links = data.rss.channel.item.map((data: any) => data.link)
+  const res = await fetch("https://www.nhk.or.jp/rss/news/cat0.xml");
+  const text = await res.text();
+  const parser = new XMLParser();
+  const data = parser.parse(text);
+  const links = data.rss.channel.item.map((data: any) => data.link);
   links.forEach((link: string) => {
-    channel.send({ content: link })
-  })
-}
+    channel.send({ content: link });
+  });
+};
+
--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,27 @@
 // Require the necessary discord.js classes
-import {
-  Client,
-  GatewayIntentBits,
-  TextChannel,
-} from "discord.js"
+import { Client, GatewayIntentBits, TextChannel } from "discord.js";
 import dotenv from "dotenv";
 import { echoRinen, sendNHKNews } from "./functions";
-dotenv.config()
-const DISCORDTOKEN = process.env.DISCORD_TOKEN
+dotenv.config();
+const DISCORDTOKEN = process.env.DISCORD_TOKEN;
 const CHANNEL_ID = process.env.CHANNEL_ID;
-if(!CHANNEL_ID) throw new Error("チャンネルIDが設定されていません");
+if (!CHANNEL_ID) throw new Error("チャンネルIDが設定されていません");
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds,GatewayIntentBits.GuildMessages] })
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
+});
 
 client.once("ready", async () => {
   console.log("Ready!");
   const channel = client.channels.cache.get(CHANNEL_ID);
-  if(!channel) {
+  if (!channel) {
     console.log("チャンネルが存在しません");
     return;
   }
-  if(channel instanceof TextChannel) {
+  if (channel instanceof TextChannel) {
     sendNHKNews(channel);
   }
-})
-
+});
 
 // Login to Discord with your client's token
 client.login(DISCORDTOKEN);
